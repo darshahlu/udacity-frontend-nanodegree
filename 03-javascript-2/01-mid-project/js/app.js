be89cc940e6a2edbc85625248240676d860eb6d6
Darshan Ahluwalia <darshahlu@gmail.com>
2018-09-16 14:28:56 -0700

    Showing game stats; showing game over screen; stopping game temporarily on win or loss.

--- 03-javascript-2/01-mid-project/js/app.js
+++ 03-javascript-2/01-mid-project/js/app.js
@@ -17,28 +17,42 @@ class GameStats {
   constructor() {
     this.wins = 0;
     this.losses = 0;
+    this.current_level = 1;
     this.max_level_reached = 0;
     this.enemies_dodged = 0;
     this.enemies_max_speed = 0;
   }
-  recordStats(all_enemies, current_level) {
-    if (current_level > this.max_level_reached) {
-      this.max_level_reached += 1;
+  update(otherGameStats) {
+    this.wins += otherGameStats.wins;
+    this.losses += otherGameStats.losses;
+    this.current_level = otherGameStats.current_level;
+    if (otherGameStats.max_level_reached > this.max_level_reached) {
+      this.max_level_reached = otherGameStats.max_level_reached;
     }
+    this.enemies_dodged += otherGameStats.enemies_dodged;
+    if (otherGameStats.enemies_max_speed > this.enemies_max_speed) {
+      this.enemies_max_speed = otherGameStats.enemies_max_speed;
+    }
+  }
+  recordStats(all_enemies) {
+    if (this.current_level > this.max_level_reached) {
+      this.max_level_reached = this.current_level;
+    }
+  }
+  recordWin(all_enemies) {
+    this.current_level += 1;
+    this.wins += 1;
     this.enemies_dodged += all_enemies.length;
     for (let enemy of all_enemies) {
       if (enemy.speed > this.enemies_max_speed) {
         this.enemies_max_speed = enemy.speed;
       }
     }
+    this.recordStats(all_enemies);
   }
-  recordWin(all_enemies, current_level) {
-    this.wins += 1;
-    this.recordStats(all_enemies, current_level);
-  }
-  recordLoss(all_enemies, current_level) {
+  recordLoss(all_enemies) {
     this.losses += 1;
-    this.recordStats(all_enemies, current_level);
+    this.recordStats(all_enemies);
   }
 }
 
@@ -50,18 +64,19 @@ class EnemyCreator {
     this.enemies_max = enemies_max;
     this.enemies = [];
   }
-  createEnemy() {
+  createEnemy(num_enemies) {
     let speed = (Math.random() * (this.enemy_speed_max - this.enemy_speed_min + 1)) + this.enemy_speed_min;
     let row_index = Math.floor(Math.random() * ENEMY_ROWS.length);
     let row = ENEMY_ROWS[row_index];
-    console.log(`Enemy created: speed=${speed}, row=${row}`);
-    let enemy = new Enemy('images/enemy-bug.png', speed, row);
+    let col = PIXELS_PER_COL * (Math.floor(Math.random() * num_enemies) + 1);
+    console.log(`Enemy created: speed=${speed}, row=${row}, col=${col}`);
+    let enemy = new Enemy('images/enemy-bug.png', speed, row, col);
     return enemy;
   }
   createEnemies() {
     let num_enemies = Math.floor(Math.random() * (this.enemies_max - this.enemies_min + 1)) + this.enemies_min;
     for (let i = 0; i < num_enemies; i++) {
-      let enemy = this.createEnemy();
+      let enemy = this.createEnemy(num_enemies);
       this.enemies.push(enemy);
     }
     return this.enemies;
@@ -70,19 +85,21 @@ class EnemyCreator {
 
 
 class Enemy {
-  constructor(sprite, speed, row) {
+  constructor(sprite, speed, row, col) {
     this.sprite = sprite;
     this.speed = speed;
     this.row_index = row;
-    this.x = INITIAL_ENEMY_POSITION;
+    this.x = col;
     this.y = this.row_index * PIXELS_PER_ROW + -23;
     // image begins at 2; ends at pixel 99
   }
   occupiedXMin() {
-    return this.x + 2;
+    // Enemy rear begins at about pixel 2 but allowing player to occupy some space behind enemy
+    return this.x + 20;
   }
   occupiedXMax() {
-    return this.x + 99; // 101 - 2
+    // Enemy front ends at about pixel 99, but allowing some space for player in front of enemy
+    return this.x + 95;
   }
   occupiedXCenter() {
     return this.occupiedXMin() + (this.occupiedXMax() - this.occupiedXMin()) / 2;
@@ -105,6 +122,7 @@ class Enemy {
 class Player {
   constructor() {
     this.sprite = 'images/char-boy.png';
+    this.movement_enabled = true;
     this.reset();
     // image begins at 17; ends at pixel 84
   }
@@ -167,16 +185,21 @@ class Player {
       this.col_index -= 1;
     }
   }
+  disableMovement() {
+    this.movement_enabled = false;
+  }
   handleInput(action_name) {
-    if (action_name == 'up') {
-      this.moveUp();
+    if (this.movement_enabled) {
+      if (action_name == 'up') {
+        this.moveUp();
 
-    } else if (action_name == 'down') {
-      this.moveDown();
-    } else if (action_name == 'right') {
-      this.moveRight();
-    } else if (action_name == 'left') {
-      this.moveLeft();
+      } else if (action_name == 'down') {
+        this.moveDown();
+      } else if (action_name == 'right') {
+        this.moveRight();
+      } else if (action_name == 'left') {
+        this.moveLeft();
+      }
     }
   }
 }
@@ -187,15 +210,20 @@ class GameState {
     this.gameStats = new GameStats();
     this.player = new Player();
     this.allEnemies = [];
-    this.level = 1;
   }
   clearEnemies() {
     while (this.allEnemies.length > 0) {
       this.allEnemies.pop();
     }
   }
-  populateEnemies(level) {
-    let enemy_creator = new EnemyCreator(this.level * 100, this.level * 100 + 100, this.level * 2, this.level * 2.5);
+  stopEnemies() {
+    for (let enemy of this.allEnemies) {
+      enemy.speed = 0;
+    }
+  }
+  populateEnemies() {
+    let level = this.gameStats.current_level;
+    let enemy_creator = new EnemyCreator(level * 50, level * 50 + 100, level * 1.25, level * 1.5);
     enemy_creator.createEnemies();
     for (let enemy of enemy_creator.enemies) {
       this.allEnemies.push(enemy);
@@ -203,24 +231,50 @@ class GameState {
   }
   startNewGame(gameResult = null) {
     if (gameResult == 'win') {
-      this.gameStats.recordWin(this.allEnemies, this.level);
-      this.level += 1;
-    } else if (gameResult == 'lose') {
-      this.gameStats.recordLoss(this.allEnemies, this.level);
-      this.level = 1;
+      this.gameStats.recordWin(this.allEnemies);
     }
     this.clearEnemies();
-    this.populateEnemies(this.level);
+    this.populateEnemies();
     this.player.reset();
+    this.updateStatsBoard();
+  }
+  gameOver() {
+    this.gameStats.recordLoss(this.allEnemies);
+    this.stopEnemies();
+    this.player.disableMovement();
+
+    allGameStats.update(this.gameStats);
+
+    sessionStorage.setItem("gameStats", JSON.stringify(allGameStats));
+
+    setTimeout(function() {
+      window.location.href = 'lose.html';
+    }, 2000, )
+  }
+  updateStatsBoard() {
+    document.querySelector("#level-display").textContent = this.gameStats.current_level;
+    document.querySelector("#wins-display").textContent = this.gameStats.wins;
+    document.querySelector("#enemies-num-display").textContent = this.gameStats.enemies_dodged;
+    document.querySelector("#enemies-speed-display").textContent = this.gameStats.enemies_max_speed;
   }
 }
 
+// Instantiate game objects.
+// fixme: this is ugly with allGameStats as a global?
+const priorGameStats = sessionStorage.getItem("gameStats");
+const allGameStats = new GameStats();
+if (priorGameStats) {
+  allGameStats.update(JSON.parse(priorGameStats));
+}
 const gameState = new GameState();
 const player = gameState.player;
 const allEnemies = gameState.allEnemies;
-gameState.startNewGame();
 
-// Event listeners
+function main(event) {
+  gameState.startNewGame();
+}
+
+// Event listeners.
 document.addEventListener('keyup', function(e) {
   let allowedKeys = {
     37: 'left',
@@ -231,3 +285,4 @@ document.addEventListener('keyup', function(e) {
 
   player.handleInput(allowedKeys[e.keyCode]);
 });
+window.onload = main;
